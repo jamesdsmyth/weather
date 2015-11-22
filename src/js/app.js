7707e01de4ea4ce6da520a3d8fbca96e883c8d12
James Smyth <smythie_89@hotmail.com>
2015-11-22 18:34:53 +0000

    adding city dropdown

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,33 @@
 var app = angular.module('weather', ['ngRoute'])
 
-app.controller('mainController', ['$scope', 'weatherFactory', function($scope, weatherFactory) {
+app.controller('mainController', ['$scope', 'weatherFactory', 'locationFactory', function ($scope, weatherFactory, locationFactory) {
     $scope.weather = null;
     weatherFactory.json().success(function(response) {
         $scope.weather = response;
     });
+
+    $scope.locations = locationFactory.list;
+    // {
+    //     'Perugia': {
+    //         country: [
+    //             "IT"
+    //          ]
+    //     },
+    //     'New Brunswick': {
+    //         state: [
+    //             "NJ"
+    //         ]
+    //     },
+    //     'Vicenza': {
+    //         country: [
+    //             "IT"
+    //         ]
+    //     }
+    // };
 }]);
 
-// Factory returns the JSON file of the city.
-app.factory('weatherFactory', ['$http', function($http) {
+// Factory returns the JSON file of the weather
+app.factory('weatherFactory', ['$http', function ($http) {
     var weatherJson = {};
     weatherJson.json = function () {
         return $http.get('http://api.openweathermap.org/data/2.5/forecast?id=2643743&APPID=d59ec95d5562a7eced9ca8454c2115e2');
@@ -16,19 +35,34 @@ app.factory('weatherFactory', ['$http', function($http) {
     return weatherJson;
 }]);
 
+// Factory returns the cities
+app.factory('locationFactory', ['$http', function ($http) {
+    var location = {};
+    location.list = [];
+
+    location.json = $http.get('../json/cities.json').success(function(response){
+        location.list.push(response);
+        // debugger;
+    }).error(function(){
+        console.log('error');
+    });
+
+    return location;
+}]);
+
 // Custom filter. Kelvin is returned so it needs to be converted to celcius. 'input' is angular standard.
-app.filter('celciusFilter', function() {
+app.filter('celciusFilter', function () {
     return function (input) {
         return input = Math.round(input - 273.15);
     }
 });
 
 // Custom filter. Gets the correct title for the image and is passed back.
-app.filter('weatherFilter', function() {
+app.filter('weatherFilter', function () {
     return function (input) {
         var weather = null;
 
-        switch(input) {
+        switch (input) {
             case 'Clear':
                 weather = 'sun';
                 break;
@@ -47,6 +81,8 @@ app.filter('weatherFilter', function() {
 //
 app.directive('weatherInterval', function(){
     return {
-        template: "<div class='james'>aass</div>"
+        restrict: 'E',
+        require: 'mainController',
+        template: "<div class='james'>{{ interval.dt * 1000 }}</div>"
     }
 });
